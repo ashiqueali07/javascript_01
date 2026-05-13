8c02abe2604f6734b9a9823dcf09f5b08d347c85
ali <ashiqueali1715@gmail.com>
2026-05-14 00:44:15 +0530

    learn variables and data types in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456
+let accountEmail ="ali123@gmail.com"
+var accountpassword ="2345678"
+accountcity ="bengluru "
+let accountstate;
+
+
+//accountId = 2
+accountEmail ="ashiqueali12@gmail.com"
+accountpassword = "3456567"
+accountcity ="varnasi"
+/*
+prefere not to use var
+because of issue in block scope and functional scope.
+*/
+console.table([accountId, accountEmail, accountpassword, accountcity, accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert( 3+ 3 ) // we are using nodejs, not browser
+
+console.log("ali");
+
+
+let name =" ali"
+let age = 20
+let isloggedIn = false
+
+
+//number => 2 to power 53
+//
+//bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+//undefined =>
+//symbol  => unique
+//object
+
+
+console.log(typeof ali);
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined);  // undefined
+
+
+
