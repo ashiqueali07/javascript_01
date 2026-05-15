9ae5afca2289f004d5abd76bd24b8952c9bd0bcb
ali <ashiqueali1715@gmail.com>
2026-05-15 22:42:31 +0530

    learn about operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -28,3 +28,42 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// ***********************OPERATIONS************************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "ali"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);  // don't write code like this
+console.log(+"");    // don't write code like this
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// read about prefix and postfix on MDN
